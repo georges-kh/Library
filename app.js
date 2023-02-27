9bd9cccaa4387de3773d6a0a43d9b72d1d145428
Georges Khoueiry <georges.nkhoueiry@gmail.com>
2023-02-27 20:34:57 +0200

    Add books Submitting the form now creates a new card with the information given but does not yet submit to a server

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// BRING FORM UP
+const add = document.querySelector("#add-book");
+const form = document.querySelector("form");
+
+add.addEventListener("click", () => {
+  form.style.display = "flex";
+});
+
+
+// CLOSE FORM
+const close = document.querySelector("#close-form");
+
+close.addEventListener("click", closeForm);
+
+function closeForm() {
+  form.style.display = "none";
+}
+
+// STORE BOOKS
 class Books {
   constructor(name, author, pages, status) {
     this.name = name;
@@ -9,6 +28,47 @@ class Books {
 
 let Library = [];
 
+// stores books in the Library object
 function addBook(book) {
+  // book: an object that contains all the information the user submitted about a book
+  // Does not return anything, stores each book in Library
   Library.push(book);
+}
+
+
+// displays each book in card on the page
+function displayBook() {
+  // Library: an array of object containing information about each  book  the user submitted
+  
+  //creates a div with the class "card" and adds it to "content"
+  let card = document.createElement("div");
+  document.querySelector(".content").appendChild(card);
+  card.classList.add("card");
+  
+  for (let i of Object.values(Library[Library.length - 1])) {
+    //creates a p element and adds the name, author, page count, and status a book and adds it to card
+    let line = document.createElement("p");
+    line.textContent = i;
+    card.appendChild(line);
+  }
+}
+
+
+// PREVENT THE FORM FROM SUBMITTING
+const submit = document.querySelector("#submit-button");
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  closeForm();
+  updateLibrary();
+  displayBook();
+})
+
+function updateLibrary() {
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  const statusValue = (document.querySelector("#status").checked) ? "Read" : "Not read";
+
+  let book = new Books(title.value, author.value, pages.value, statusValue);
+  addBook(book);
 }
